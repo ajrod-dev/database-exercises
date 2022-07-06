246432d558348b98fe9cc07f9477aa36cd456442
Alex Rodriguez <alexander.joshua.rodriguez@gmail.com>
2022-07-06 16:48:09 -0500

    update to table

---
+++ albums_seeder.sql
@@ -0,0 +1,12 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, song, release_date, sales, genre)
+VALUES ('Michael Jackson','none',1982,70,'pop'),
+        ('AC/DC','none',1980,50,'hard rock'),
+        ('Whitney Houston','none',1992,45,'soul'),
+        ('The Eagles','none',1976,44, 'soft rock'),
+        ('Pink Floyd','none',1973,44,'rock'),
+        ('Shania Twain','none',1997,40,'country'),
+        ('Fleetwood Mac','none',1977,40,'soft rock'),
+        ('The Bee Gees','none',1977,40,'disco');
+
