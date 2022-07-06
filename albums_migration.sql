0243cad5d41928bd523894cd813d0c67daebd202
Alex Rodriguez <alexander.joshua.rodriguez@gmail.com>
2022-07-06 13:46:56 -0500

    create table exercise

---
+++ albums_migration.sql
@@ -0,0 +1,14 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50) NOT NULL,
+    song VARCHAR(50) NOT NULL,
+    release_date INT UNSIGNED NOT NULL,
+    sales DECIMAL(4,2) UNSIGNED,
+    genre VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+
