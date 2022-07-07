6225ea44aeba3f5afa61d0384a1ee29f9aa1e434
Alex Rodriguez <alexander.joshua.rodriguez@gmail.com>
2022-07-07 09:29:42 -0500

    select_exercises

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,3 +1,20 @@
 USE codeup_test_db;
 
-SELECT * FROM albums WHERE artist = 'Pink Floyd';
+    -- The name of all albums by Pink Floyd.
+SELECT 'Pink Floyd albums' AS 'Caption';
+SELECT name FROM albums WHERE artist = 'Pink Floyd';
+    /*The year Sgt. Pepper's Lonely Hearts Club Band was released */
+SELECT 'Sgt. Peppers Lonely Hearts Club Band release date' AS 'Caption';
+SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+    -- The genre for Nevermind
+SELECT 'The genre for Nevermind' AS 'Caption';
+SELECT genre FROM albums WHERE name = 'Nevermind';
+    -- Which albums were released in the 1990s
+SELECT 'Albums released in the 90s' AS 'Caption';
+SELECT name FROM albums WHERE release_date >= 1990;
+    -- Which albums had less than 20 million certified sales
+SELECT 'Albums with less than 20 mill in sales' AS 'Caption';
+SELECT name FROM albums WHERE sales < 20;
+    -- All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
+SELECT 'Albums with genre as rock' AS 'Caption';
+SELECT name FROM albums WHERE genre = 'Rock';
